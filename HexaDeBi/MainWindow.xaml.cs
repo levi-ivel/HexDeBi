ceb826e68bdb112b7ef4054383953de606c61622
levi <142150222+levi-ivel@users.noreply.github.com>
2023-09-26 14:24:09 +0200

    Add files via upload

---
+++ HexaDeBi/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HexaDeBi
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConvertButton_Click(object sender, RoutedEventArgs e)
+        {
+            string TextInput = InputText.Text;
+
+            foreach (char c in TextInput)
+                Console.WriteLine(Convert.ToString(c, 2).PadLeft(8, '0'));
+            Console.ReadLine();
+
+            binaryText.Text = "Binary: " + Console.ReadLine();
+        }
+    }
+}
